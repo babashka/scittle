a376ffb66b11ae9c8426ceb00458db7a765e7b8c
kloimhardt <kloimhardt@kloimhardts-MacBook-Air.local>
2021-06-08 21:44:31 +0200

    use official qlkit version

---
+++ src/scittle/qlkit-externs.js
@@ -0,0 +1,2 @@
+var Component = {};
+Component.setState = function () {};
--- src/scittle/qlkit.cljs
+++ src/scittle/qlkit.cljs
@@ -5,8 +5,22 @@
 
 (def qns (sci/create-ns 'qlkit.core nil))
 
+;;directly copy-pasted from sourcecode of qlkit.core
 (defn ^:macro defcomponent* [form env nam & bodies]
-  (apply ql/defcomponent+ form env nam bodies))
+     (doseq [[nam] bodies]
+       (when-not ('#{state query render component-did-mount component-will-unmount component-will-receive-props} nam)
+         (throw (ex-info (str "Unknown component member " nam) {}))))
+     `(let [key# (keyword ~(str (:name (:ns env))) ~(name nam))] ;;env is {} in scittle, thus (:ns env) is nil, thus component names are not namespaced names
+        (def ~nam key#)
+        (#'qlkit.core/add-class key#
+                     ~(into {:display-name (name nam)}
+                            (for [[nam & more :as body] bodies]
+                              (if ('#{state query} nam)
+                                [(keyword nam)
+                                 (last more)]
+                                [(keyword nam)
+                                 `(fn ~(first more)
+                                    ~@(rest more))]))))))
 
 (def qlkit-namespace
   {'add-class (sci/copy-var ql/add-class qns)
