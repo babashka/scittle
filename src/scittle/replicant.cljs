a1b2723ef67ee81d4fc8bc94fe2ea1fa7e26c5bf
Jeroen van Dijk <27645+jeroenvandijk@users.noreply.github.com>
2025-06-16 16:31:38 +0300

    Use replicant via sci.configs (#109)
    * Update Replicant plugin to use sci.configs
    
    * Update Replicant to 2025.03.27

--- src/scittle/replicant.cljs
+++ src/scittle/replicant.cljs
@@ -1,8 +1,8 @@
 (ns scittle.replicant
   (:require
-   [sci.configs.replicant.replicant-dom :refer [replicant-dom-namespace]]
+   [sci.configs.cjohansen.replicant :refer [config]]
    [scittle.core :as scittle]))
 
 (scittle/register-plugin!
  ::replicant
- {:namespaces {'replicant.dom replicant-dom-namespace}})
+ config)
